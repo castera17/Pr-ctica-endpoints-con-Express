50472d6b05a181fe40a5a202c1dc1b24ddce1782
castera17 <48385887@est.ort.edu.ar>
2025-04-11 10:40:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import express, { json } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido a mi servidor');
+})
+
+app.get('/saludo', (req, res) => {
+    res.send('¡Hola, mundo!')
+})
+
+app.get('/numero', (req, res) => {
+    res.send('42')
+})
+
+app.get('/usuario', (req, res) => {
+    res.json({"nombre": "Ana", "edad": 25})
+})
+
+app.get('/productos', (req, res) => {
+    res.json(["Mouse", "Teclado", "Monitor"])
+})
+
+app.get('/materias', (req, res) => {
+    res.json([{"nombre": "Matematica"},{"nombre": "Lengua"}])
+})
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+})
+

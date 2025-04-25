6ea803f44141185586b9404ce32a799fa66e85bf
MAT�AS JOAQUIN CASTERA <48385887@ort>
2025-04-25 10:28:26 -0300

    shha

--- index.js
+++ index.js
@@ -31,14 +31,28 @@ app.get('/materias', (req, res) => {
 })
 
 
-const persona = [];
+const personas = [{}];
 
 app.post('/personas', (req, res) => {
     const persona = req.body;
     if (!persona) return res.status(400).json({ error: 'Faltan datos de la persona' });
 })
 
+app.delete('/personas/:indice', (req, res) => {
+    const indice = parseInt(req.params.indice);
+  
+    if (isNaN(indice) || !personas[indice]) {
+      return res.status(404).json({ error: 'Índice no válido' });
+    }
+  
+    const eliminado = personas.splice(indice, 1);
+    res.json({ mensaje: `Persona eliminada: ${eliminado[0].nombre}`, personas });
+  });
+
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
 })
+
+
+
 
